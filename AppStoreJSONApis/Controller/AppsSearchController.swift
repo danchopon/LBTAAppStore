59da0a43df2d44405d60294044549dba499e6ca7
Daniyar Erkinov <dan4111k1@gmail.com>
2019-07-02 13:33:49 +0600

    add search bar

--- AppStoreJSONApis/Controller/AppsSearchController.swift
+++ AppStoreJSONApis/Controller/AppsSearchController.swift
@@ -13,19 +13,42 @@ class AppsSearchController: UICollectionViewController {
   
   fileprivate let cellId = "id1234"
   
+  var timer: Timer?
+  
+  fileprivate let searchController = UISearchController(searchResultsController: nil)
+  
+  fileprivate let enterSearchTermLabel: UILabel = {
+    let label = UILabel()
+    label.text = "Please enter search term above..."
+    label.textAlignment = .center
+    label.font = UIFont.boldSystemFont(ofSize: 20)
+    return label
+  }()
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     
     collectionView.backgroundColor = .white
     collectionView.register(SearchResultCell.self, forCellWithReuseIdentifier: cellId)
     
-    fetchITunesApps()
+    collectionView.addSubview(enterSearchTermLabel)
+    enterSearchTermLabel.fillSuperview(padding: .init(top: 100, left: 50, bottom: 0, right: 50))
+    
+    setupSearchBar()
+  }
+  
+  fileprivate func setupSearchBar() {
+    definesPresentationContext = true
+    navigationItem.searchController = self.searchController
+    navigationItem.hidesSearchBarWhenScrolling = false
+    searchController.dimsBackgroundDuringPresentation = false
+    searchController.searchBar.delegate = self
   }
   
   fileprivate var appResults = [Result]()
   
   fileprivate func fetchITunesApps() {
-    Service.shared.fetchApps { (results, err) in
+    Service.shared.fetchApps(searchTerm: "twitter") { (results, err) in
       if let err = err {
         print("Failed to fetch apps:", err)
         return
@@ -46,6 +69,7 @@ class AppsSearchController: UICollectionViewController {
   }
   
   override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    enterSearchTermLabel.isHidden = appResults.count != 0
     return appResults.count
   }
   
@@ -60,5 +84,22 @@ class AppsSearchController: UICollectionViewController {
 extension AppsSearchController: UICollectionViewDelegateFlowLayout {
   func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
     return .init(width: view.frame.width, height: 325)
+  }
+}
+
+extension AppsSearchController: UISearchBarDelegate {
+  
+  func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+    print(searchText)
+    
+    timer?.invalidate()
+    timer = Timer.scheduledTimer(withTimeInterval: 0.5, repeats: false, block: { (_) in
+      Service.shared.fetchApps(searchTerm: searchText) { (res, err) in
+        self.appResults = res
+        DispatchQueue.main.async {
+          self.collectionView.reloadData()
+        }
+      }
+    })
   }
 }
